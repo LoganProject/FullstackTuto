08ddfa8c59b5850bbaa97c89843f3f01dd1d4800
Logan Faraud <logan.faraud@gmail.com>
2025-11-26 16:27:18 +0100

    Add express

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const http = require("http");
+import http from 'http';
+import { app } from "./app.js";
 
-const server = http.createServer((req, res) => {
-    res.end('Réponse du serveur');
-})
+app.set("port", process.env.PORT || 3000);
+const server = http.createServer(app)
 
-server.listen(3000);
+server.listen(process.env.PORT ||3000);
