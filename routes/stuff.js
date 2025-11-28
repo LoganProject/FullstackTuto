2cd6c035390b2a36cbe05359c49c90641338edb4
Logan Faraud <logan.faraud@gmail.com>
2025-11-28 17:29:19 +0100

    Implement the auth via token to modify bd for stuff - End of Part 3

--- routes/stuff.js
+++ routes/stuff.js
@@ -1,14 +1,15 @@
 import express from "express";
 import {createThing, deleteOneThing, getAllThings, getOneThing, modifyThing} from "../controllers/stuff.js";
+import {middleware as auth} from "../middleware/auth.js";
 
 export const router = express.Router();
 
-router.get('/', getAllThings);
+router.get('/', auth, getAllThings);
 
-router.get('/:id', getOneThing)
+router.get('/:id', auth, getOneThing)
 
-router.post('/', createThing)
+router.post('/', auth, createThing)
 
-router.put('/:id', modifyThing)
+router.put('/:id', auth, modifyThing)
 
-router.delete('/:id', deleteOneThing)
+router.delete('/:id', auth, deleteOneThing)
